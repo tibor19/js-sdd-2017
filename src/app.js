5421bbbf6f553c9374bbd824624e94e7994eb373
Tiberiu Covaci <tibi@covaci.se>
2017-05-15 14:34:15 +0100

    Demo Let

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-let a = 10;
+function scopeLet(){
+    let ab = 20;
+    if(true){
+        let ab = 10;
+        console.log("Let inner ab ", ab);
+    }
+    console.log("Let outter ab ", ab);
+}
+
+function scopeVar(){
+    var ab = 20;
+    if(true){
+        var ab = 10;
+        console.log("Var inner ab ", ab);
+    }
+    console.log("Var outter ab ", ab);
+}
+
+const c = 11;
+
+function run(){
+    
+    scopeLet();
+    scopeVar();
+    console.log(c);
+}
+
+run();
