62aca6a11457bdccc397770a8b7a2d0d6dba1c3b
Tiberiu Covaci <tibi@covaci.se>
2017-05-15 14:36:20 +0100

    Destructurin objects

--- src/app.js
+++ src/app.js
@@ -26,3 +26,31 @@ function run(){
 }
 
 run();
+
+let person = {
+    name: 'Tibi',
+    age: 44,
+    address: {
+        city: 'Tenerife',
+        country: 'Spain'
+    }
+}
+
+let {name: n, address: {city, zip = 90210}} 
+            = person;
+
+function sendMail({name, address: {city, zip}}){
+    console.log(name, city);
+    console.log(zip);
+}
+
+let arr = [1, 2, 3, 4];
+
+let [, a, b, , d] = arr;
+// let [e, f] = person;
+
+function runDestr(){
+    sendMail(person);
+}
+
+runDestr();
